3ba2b7755e6b6cb796044e5c3a4dcafad13b04f3
unknown <arishardisantoso@gmail.com>
2023-06-13 14:30:03 +0700

    Update Multiple Number

--- src/check.js
+++ src/check.js
@@ -2,10 +2,11 @@ const chalk = require("chalk");
 const log = console.log;
 const checkNumber = require("./utils/checkNumber");
 const checkNumberBulk = require("./utils/checkNumberBulk");
-const Numbers = [6283829949707,6283829943333,6289669235897];
+const Numbers = [];
+// const Numbers = [6283829949707,6283829943333,6289669235897];
 
 (async () => {
-	console.log("cek",process.argv);
+	console.log("cek", process.argv);
 	if (!Numbers && process.argv.length < 3) {
 		log(chalk.red.bold("No Phone Number has been passed"));
 		log(chalk.blue("Example: npm run check 919898989898"));
@@ -27,10 +28,31 @@ const Numbers = [6283829949707,6283829943333,6289669235897];
 	// 	process.exit(0);
 
 	// }
-	const phoneNumber = process.argv[2];
-	log(chalk.blue(`Checking for Existence : ${phoneNumber}`));
-	const numberExists = await checkNumber(phoneNumber);
-	if (numberExists) log(chalk.green.bold("Number Exists on Whatsapp"));
-	else log(chalk.red.bold("Number doesn't exist on Whatsapp"));
-	process.exit(0);
+
+	if (Numbers.length > 0) {
+		for (const iterator of Numbers) {
+			const numberIsExist = await checkNumber(iterator)
+			if (numberIsExist) {
+				log(chalk.green.bold(`Number ${iterator} is Exist`))
+			} else {
+				log(chalk.red.bold(`Number ${iterator} doesn't exist`))
+			}
+		}
+	} else {
+
+		
+		const phoneNumber = process.argv[2];
+		
+		if (phoneNumber) {
+			log(chalk.blue(`Checking for Existence : ${phoneNumber}`));
+			const numberExists = await checkNumber(phoneNumber);
+			if (numberExists) log(chalk.green.bold("Number Exists on Whatsapp"));
+			else log(chalk.red.bold("Number doesn't exist on Whatsapp"));
+		} else {
+			log(chalk.red.bold("No Phone Number has been passed"));
+			log(chalk.blue("Example: npm run check 919898989898"));
+		}
+
+		process.exit(0);
+	}
 })();
