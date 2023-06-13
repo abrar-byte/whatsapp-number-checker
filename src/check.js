1b90d32b7666e959aed0c46e4384cb18862791c7
abrar-byte <usamahhafidz99@gmail.com>
2023-06-13 13:21:20 +0700

    first commit

---
+++ src/check.js
@@ -0,0 +1,36 @@
+const chalk = require("chalk");
+const log = console.log;
+const checkNumber = require("./utils/checkNumber");
+const checkNumberBulk = require("./utils/checkNumberBulk");
+const Numbers = [6283829949707,6283829943333,6289669235897];
+
+(async () => {
+	console.log("cek",process.argv);
+	if (!Numbers && process.argv.length < 3) {
+		log(chalk.red.bold("No Phone Number has been passed"));
+		log(chalk.blue("Example: npm run check 919898989898"));
+		process.exit(0);
+	}
+	// if (Numbers) {
+	// 	log(chalk.blue.bold("Checking..."));
+	// 	for (let i = 0; i <= Numbers.length; i++) {
+	// 		var phoneNumber = Numbers[i];
+	// 		const numberExists = await checkNumberBulk(phoneNumber);
+	// 		if (numberExists) {
+	// 			log(chalk.green.bold(`Number Exists on Whatsapp`));
+	// 			// logger.write("wa.me/" + phoneNumber)
+	// 			console.log("wa.me/" + phoneNumber);
+	// 		} else {
+	// 			console.log("false " + phoneNumber);
+	// 		}
+	// 	}
+	// 	process.exit(0);
+
+	// }
+	const phoneNumber = process.argv[2];
+	log(chalk.blue(`Checking for Existence : ${phoneNumber}`));
+	const numberExists = await checkNumber(phoneNumber);
+	if (numberExists) log(chalk.green.bold("Number Exists on Whatsapp"));
+	else log(chalk.red.bold("Number doesn't exist on Whatsapp"));
+	process.exit(0);
+})();
